4e502cda657efc15f7ac0a3c28cd9120a5f824f1
June Kim <jooeun@caltech.edu>
2021-02-08 12:52:43 -0800

    Add full power up initialization to Cpu constructor

--- Game Boy Emulator/Cpu.cpp
+++ Game Boy Emulator/Cpu.cpp
@@ -9,9 +9,47 @@ using namespace std;
 
 
 Cpu::Cpu(Memory& memory) : _memory(memory), _registers(Registers{}) {
-    // Power up - initialize program counter and stack pointer 
+    // Power up (reset register values)
+    // Initialize program counter and stack pointer 
     _registers.set_pc(0x100);
     _registers.set_sp(0xFFFE);
+
+    _registers.set_af(0x01B0);
+    _registers.set_bc(0x0013);
+    _registers.set_de(0x00D8);
+    _registers.set_hl(0x014D);
+
+    _memory.write(0xFF05, 0x00);    // TIMA 
+    _memory.write(0xFF06, 0x00);    // TMA
+    _memory.write(0xFF07, 0x00);    // TAC
+    _memory.write(0xFF10, 0x80);    // NR10
+    _memory.write(0xFF11, 0xBF);    // NR11
+    _memory.write(0xFF12, 0xF3);    // NR12
+    _memory.write(0xFF14, 0xBF);    // NR14
+    _memory.write(0xFF16, 0x3F);    // NR21
+    _memory.write(0xFF17, 0x00);    // NR22
+    _memory.write(0xFF19, 0xBF);    // NR24
+    _memory.write(0xFF1A, 0x7F);    // NR30
+    _memory.write(0xFF1B, 0xFF);    // NR31
+    _memory.write(0xFF1C, 0x9F);    // NR32
+    _memory.write(0xFF1E, 0xBF);    // NR34
+    _memory.write(0xFF20, 0xFF);    // NR41
+    _memory.write(0xFF21, 0x00);    // NR42
+    _memory.write(0xFF22, 0x00);    // NR43
+    _memory.write(0xFF23, 0xBF);    // NR44
+    _memory.write(0xFF24, 0x77);    // NR50
+    _memory.write(0xFF25, 0xF3);    // NR51
+    _memory.write(0xFF26, 0xF1);    // NR52
+    _memory.write(0xFF40, 0x91);    // LCDC
+    _memory.write(0xFF42, 0x00);    // SCY
+    _memory.write(0xFF43, 0x00);    // SCX
+    _memory.write(0xFF45, 0x00);    // LYC
+    _memory.write(0xFF47, 0xFC);    // BGP
+    _memory.write(0xFF48, 0xFF);    // 0BP0
+    _memory.write(0xFF49, 0xFF);    // 0BP1
+    _memory.write(0xFF4A, 0x00);    // WY
+    _memory.write(0xFF4B, 0x00);    // WX
+    _memory.write(0xFFFF, 0x00);    // IE
 }
 
 
