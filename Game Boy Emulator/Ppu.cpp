923d3b43cb3c6f3a37fda85f05e6c550e91591da
June Kim <jooeun@caltech.edu>
2021-03-18 22:18:20 -0700

    Implement displaying sprites
    Emulator now passes dmg-acid2 PPU test

--- Game Boy Emulator/Ppu.cpp
+++ Game Boy Emulator/Ppu.cpp
@@ -1,4 +1,6 @@
+#include <cassert>
 #include <iostream>
+#include <list>
 
 #include "Ppu.h"
 #include "utils.h"
@@ -9,6 +11,7 @@ using namespace std;
 
 Ppu::Ppu(Memory& memory) : _memory(memory) {
     _curr_scanline_cycles = 0;
+    _window_line = 0;
 
     _pixel_matrix = new u2[144 * 160]{ 0 };
 
@@ -53,6 +56,9 @@ void Ppu::update(const unsigned int cycles) {
     }
 
     _curr_scanline_cycles += cycles;
+    
+    // Used so that LY=LYC is checked only when it is a new scanline
+    bool is_new_scanline = false;
 
     if (_curr_scanline_cycles >= CYCLES_PER_SCANLINE) {
         // Draw current scanline before moving on to the next scaline
@@ -67,11 +73,13 @@ void Ppu::update(const unsigned int cycles) {
 
         // Update LY register value
         _memory.write(0xFF44, (curr_scanline + 1) % 154);
+
+        is_new_scanline = true;
     }
 
     // TODO: Is it ok to check LYC == LY after updating LY? or do it before and after?
     // This is probably ok..
-    _set_lcd_status();
+    _set_lcd_status(is_new_scanline);
 }
 
 
@@ -109,7 +117,7 @@ void Ppu::render() {
 }
 
 
-void Ppu::_set_lcd_status() {
+void Ppu::_set_lcd_status(const bool is_new_scanline) {
     // Get LY register value and set STAT
     u8 ly = _memory.read(0xFF44);
     u8 stat = _memory.read(0xFF41);
@@ -121,6 +129,9 @@ void Ppu::_set_lcd_status() {
         // V-Blank (Mode 1)
         curr_mode = 1;
         is_request_enabled = get_bit(stat, 4);
+        
+        // Reset window internal line counter
+        _window_line = 0;
     }
     else {
         // Check cycles elapsed during the current scanline to find which mode we're in
@@ -145,18 +156,20 @@ void Ppu::_set_lcd_status() {
         _memory.request_interrupt(1);
     }
 
-    // Update coicidence flag and request STAT interrupt if needed
-    u8 lyc = _memory.read(0xFF45);
+    // Update coincidence flag and request STAT interrupt if needed
+    if (is_new_scanline) {
+        u8 lyc = _memory.read(0xFF45);
 
-    if (ly == lyc) {
-        stat = set_bit(stat, 2, 1);
+        if (ly == lyc) {
+            stat = set_bit(stat, 2, 1);
 
-        if (get_bit(stat, 6)) {
-            _memory.request_interrupt(1);
+            if (get_bit(stat, 6)) {
+                _memory.request_interrupt(1);
+            }
         }
-    }
-    else {
-        stat = set_bit(stat, 2, 0);
+        else {
+            stat = set_bit(stat, 2, 0);
+        }
     }
 
     // Update STAT register value
@@ -207,7 +220,7 @@ void Ppu::_draw_background(const unsigned int y, const u8 lcdc) {
     bool is_using_window = get_bit(lcdc, 5) && (y >= wy) && (wx >= 0 && wx <= 166);
 
     // Draw scanline
-    for (int x = 0; x < 144; x++) {
+    for (int x = 0; x < 160; x++) {
         // Check if background or window should be displayed at this pixel
         bool is_in_window = is_using_window && (x >= (i8)(wx - 7));
 
@@ -228,7 +241,8 @@ void Ppu::_draw_background(const unsigned int y, const u8 lcdc) {
 
             // Find the x, y coordinate of the pixel in window space
             translated_x = x - (wx - 7);
-            translated_y = y - wy;
+            //translated_y = y - wy;
+            translated_y = _window_line;
         }
         else {
             if (get_bit(lcdc, 3)) {
@@ -282,9 +296,194 @@ void Ppu::_draw_background(const unsigned int y, const u8 lcdc) {
         // Draw pixel
         _pixel_matrix[y * 160 + x] = color_value;
     }
+
+    // Increment window scanline counter
+    if (is_using_window) {
+        _window_line++;
+    }
+}
+
+
+struct Sprite {
+    i8 x_pos;					// x position of the leftmost part of the sprite
+    i8 y_pos;                   // y position of the topmost part of the sprite
+    unsigned int sprite_index;	// Ranges from 0 to 39
+    u8 tile_num;
+    u8 attributes;
+};
+
+
+// Returns true if first has a higher priority than second, false otherwise.
+bool compare_sprite_priority(const Sprite& first, const Sprite& second) {
+    if (first.x_pos == second.x_pos) {
+        return (first.sprite_index < second.sprite_index);
+    }
+
+    return (first.x_pos < second.x_pos);
 }
 
 
 void Ppu::_draw_objects(const unsigned int y, const u8 lcdc) {
+    // Check if sprites are 8x8 tiles or 8x16 tiles
+    unsigned int sprite_size_y;
 
+    if (get_bit(lcdc, 2)) {
+        sprite_size_y = 16;
+    }
+    else {
+        sprite_size_y = 8;
+    }
+
+    // Scan all 40 sprites and find the first 10 sprites in the current scanline,
+    // discarding unused sprites in the scanline
+
+    // Number of sprites in this scanline
+    unsigned int num_sprites = 0;
+
+    // List of 10 Sprite objects to be displayed in the scanline
+    list<Sprite> sprites;
+
+    for (unsigned int sprite_index = 0; sprite_index < 40; sprite_index++) {
+        // Address for sprite attribute
+        u16 sprite_addr = 0xFE00 + sprite_index * 4;
+
+        // x, y positions of the top left corner of the sprite
+        i8 sprite_y = _memory.read(sprite_addr) - 16;
+        i8 sprite_x = _memory.read(sprite_addr + 1) - 8;
+        u8 tile_num = _memory.read(sprite_addr + 2);
+        u8 attributes = _memory.read(sprite_addr + 3);
+
+        if (y >= sprite_y && y < sprite_y + sprite_size_y) {
+            if (num_sprites < 10) {
+                num_sprites++;
+
+                // Create a Sprite object and add to sprites list
+                sprites.push_back(Sprite{ sprite_x, sprite_y, sprite_index, tile_num, attributes });
+            }
+            else {
+                // Discard the sprite - we cannot display this sprite
+                // Set Y Position to 0 to hide the sprite.
+                _memory.write(sprite_addr, 0);
+            }
+        }
+    }
+
+    // Sort sprites in order of highest priority to lowest priority
+    sprites.sort(compare_sprite_priority);
+
+    // Temporary array of length 160 to hold sprite pixels drawn in the scanline
+    // We need to store both color numbers and color values because there is no way 
+    // to differentiate color number 0 (transparent) from color value 0 (white color)
+    // TODO: color_numbers can just be 0 or 1 indicating transparency (whether it was drawn)
+    u2 pixel_color_numbers[160]{ 0 };
+    u2 pixel_color_values[160]{ 0 };
+    
+    // Array of length 160 that reflects the OBJ-to-BG priority attribute of 
+    // the highest priority sprite (0 means draw over BG, 1 means do not)
+    bool bg_priority[160]{ 0 };
+
+    // Draw sprites, respecting the z-axis priority
+    for (const Sprite& sprite : sprites) {
+        // Get sprite attributes
+        bool is_behind_background = get_bit(sprite.attributes, 7);
+        bool y_flip = get_bit(sprite.attributes, 6);
+        bool x_flip = get_bit(sprite.attributes, 5);
+        bool palette_num = get_bit(sprite.attributes, 4);     // OBP0 or OBP1?
+
+        // Get the scanline's corresponding row within tile 
+        u3 tile_row;
+
+        if (!y_flip) {
+            tile_row = y - sprite.y_pos;
+        }
+        else {
+            tile_row = (sprite_size_y - 1) - (y - sprite.y_pos);
+        }
+
+        // Get line address by properly finding tile_num and tile_row within 
+        // upper or lower tile if the tile is 8x16.
+        u8 tile_num;
+
+        if (sprite_size_y == 8) {
+            tile_num = sprite.tile_num;
+        }
+        else {
+            if (tile_row < 8) {     // Upper tile
+                tile_num = sprite.tile_num & 0b11111110;
+            }
+            else {                  // Lower tile
+                tile_row = tile_row - 8;
+                tile_num = sprite.tile_num | 1;
+            }
+        }
+        u16 line_addr = 0x8000 + tile_num * 16 + tile_row * 2;
+
+        // Render row
+        for (u3 x = 0; x < 8; x++) {
+            // Find col within tile
+            u3 tile_col;
+
+            if (!x_flip) {
+                tile_col = x;
+            }
+            else {
+                tile_col = 7 - x;
+            }
+
+            // Get x position in LCD
+            i8 x_pos = sprite.x_pos + x;
+
+            if (x_pos < 0 || x_pos >= 160) {
+                continue;
+            }
+
+            // Don't draw over a sprite with a higher priority
+            if (pixel_color_numbers[x_pos] != 0) {
+                continue;
+            }
+
+            // The current sprite is the highest priority sprite at this pixel.
+            // Set the OBJ-to-BG priority for the pixel
+            bg_priority[x_pos] = is_behind_background;
+
+            // Get color number
+            u3 pixel_bit = 7 - tile_col;
+
+            // Get low and high bits of color number 
+            bool color_number_low = get_bit(_memory.read(line_addr), pixel_bit);
+            bool color_number_high = get_bit(_memory.read(line_addr + 1), pixel_bit);
+
+            u2 color_num = (color_number_high << 1) | color_number_low;
+
+            // Color number 0 is transparent
+            if (color_num != 0) {
+                pixel_color_numbers[x_pos] = color_num;
+
+                // Get color value from object palette (OBP0 or OBP1)
+                u8 obp;
+
+                if (palette_num == 0) {
+                    obp = _memory.read(0xFF48);
+                }
+                else {
+                    obp = _memory.read(0xFF49);
+                }
+
+                u2 color_value = 0b11 & (obp >> (color_num * 2));
+
+                // Draw pixel
+                pixel_color_values[x_pos] = color_value;
+            }
+        }
+    }
+
+    // Draw scanline, drawing sprites on top of background if they have priority over background
+    for (int x = 0; x < 160; x++) {
+        if (pixel_color_numbers[x] != 0) {
+            // Draw over background if object has priority or if background has color 0
+            if (bg_priority[x] == 0 || _pixel_matrix[y * 160 + x] == 0) {
+                _pixel_matrix[y * 160 + x] = pixel_color_values[x];
+            }
+        }
+    }
 }
--- Game Boy Emulator/Ppu.h
+++ Game Boy Emulator/Ppu.h
@@ -20,14 +20,17 @@ private:
 	// Number of cycles elapsed in the current scan line, which gets reset at CYCLES_PER_SCANLINE
 	unsigned int _curr_scanline_cycles;
 
-	// 160 x 144 pixel matrix, where each element is a color value 0, 1, 2, or 3.
+	// Internal window line counter (line within window)
+	unsigned int _window_line;
+
+	// Pixel matrix of 144 rows by 160 columns, where each element is a color value 0, 1, 2, or 3.
 	u2* _pixel_matrix;
 
 	//SDL_Event _event;
 	SDL_Window* _window;
 	SDL_Renderer* _renderer;
 
-	void _set_lcd_status();
+	void _set_lcd_status(const bool is_new_scanline);
 	void _draw_scanline(const unsigned int scanline);
 
 	// Draws background and window for the scanline
@@ -36,4 +39,3 @@ private:
 	// Draws objects (sprites) for the scanline
 	void _draw_objects(const unsigned int scanline, const u8 lcdc);
 };
-
